c64236f315fa676f862a3954a26873d2dc1aa620
Jalo D. Capa <jalo.capa@gmail.com>
2016-03-30 12:00:01 +0200

    Finished element_isotopic_data()

--- periodic_table/nuclides.py
+++ periodic_table/nuclides.py
@@ -40,27 +40,41 @@ class PeriodicTable(object):
          which in turn are dictionaries containing masses and abundances.
         '''
         re_atomic_number = re.compile('Atomic Number = (\d+)')
+        re_atomic_symbol = re.compile('Atomic Symbol = (\w+)')
         re_mass_number = re.compile('Mass Number = (\d+)')
         re_atomic_mass = re.compile('Relative Atomic Mass = ([\d.]+)')
         re_isotopic_abundance = re.compile('Isotopic Composition = ([\d.]*)')
         re_atomic_weight = re.compile('Standard Atomic Weight = ([\[\d.,\]]+)')
         isotopes = {}
+        mass_number = 0
+        atomic_mass = 0.0
+        atomic_symbol = ''
+        isotopic_abundance = 0.0
+        tmp_aw = ''
         read_flag = False
         standard_status = -1
         for line in self.data:
             # Searching for the right element
             if re_atomic_number.search(line):
                 if int(re_atomic_number.search(line).group(1)) == atomic_num:
+                    if mass_number:
+                        isotopes[mass_number] = {
+                            'atomic number': atomic_num,
+                            'atomic symbol': atomic_symbol,
+                            'atomic mass': atomic_mass,
+                            'isotopic abundance': isotopic_abundance}
                     read_flag = True
                     mass_number = 0
                     atomic_mass = 0.0
+                    atomic_symbol = ''
                     isotopic_abundance = 0.0
-                    atomic_weight = 0.0
                     continue
             if not read_flag:
                 continue
-
             # Searching for properties
+            if re_atomic_symbol.search(line):
+                atomic_symbol = re_atomic_symbol.search(line).group(1)
+                continue
             if re_mass_number.search(line):
                 mass_number = int(re_mass_number.search(line).group(1))
                 continue
@@ -68,19 +82,49 @@ class PeriodicTable(object):
                 atomic_mass = float(re_atomic_mass.search(line).group(1))
                 continue
             if re_isotopic_abundance.search(line):
-                ab_str = re_isotopic_abundance.search(line).group(1)
-                if ab_str:
-                    isotopic_abundance = float(ab_str)
+                tmp_isoab = re_isotopic_abundance.search(line).group(1)
+                if tmp_isoab:
+                    isotopic_abundance = float(tmp_isoab)
                 else:
                     isotopic_abundance = 0.0
                 continue
-
             # Element properties
-            if standard_status > -1:
-                if (re_mass_number.search(line)):
-                    pass
-                if (re_mass_number.search(line)):
-                    pass
+            if (re_atomic_weight.search(line)):
+                if standard_status == -1:
+                    tmp_aw = re_atomic_weight.search(line).group(1)
+                    standard_status = 0
+                read_flag = False
+                continue
+        if tmp_aw:
+            isotopes = self.atomic_weight(isotopes, tmp_aw)
+        return isotopes
+
+    def atomic_weight(self, isotopes, tmp_aw):
+        '''
+        Adds an 'atomic weight' and 'stable' key to the isotopes dict.
+        '''
+        if ',' in tmp_aw:
+            # If there is a range, we should assign the average
+            re_num = re.compile('\[(\d.+),(\d.+)\]')
+            masses = [re_num.match(tmp_aw).group(1),
+                      re_num.match(tmp_aw).group(2)]
+            isotopes['atomic weight'] = (float(masses[0]) +
+                                         float(masses[1])) / 2
+            isotopes['stable'] = True
+        elif '[' in tmp_aw:
+            # We should assign the mass of the most stable isotope
+            masses = [value['atomic mass'] for value in isotopes.values()]
+            ma = int(re.match('\[(\d+)\]', tmp_aw).group(1))
+            isotopes['atomic weight'] = min(masses, key=lambda x: abs(x - ma))
+            isotopes['stable'] = False
+        else:
+            try:
+                # This is just a number
+                isotopes['atomic weight'] = float(tmp_aw)
+                isotopes['stable'] = True
+            except:
+                print 'Could not convert the Standard Atomic Weight'
+        return isotopes
 
     def read_data_file(self):
         """
