7e268a1e8fbd6e5ef4e617f661ef355a40fbb1d1
Jalo D. Capa <jalo.capa@gmail.com>
2016-05-01 23:45:02 +0200

    Fixed PEP warnings.

--- periodic_table/periodictable.py
+++ periodic_table/periodictable.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 from __future__ import print_function
 from __future__ import division
 # from __future__ import unicode_literals  # This breaks the rb+ writing
@@ -20,11 +18,13 @@ HDF5FILE = os.path.join(here, "data", "2015_mass_weight_abundance.hdf5")
 
 class Nuclide(object):
     """
-    This contains properties of a given nuclide
-      Var: atomic_symbol, atomic_number, mass_number, atomic_mass, abundance
+    This contains properties of a given nuclide.
+
+    Var: atomic_symbol, atomic_number, mass_number, atomic_mass, abundance
     """
 
     def __init__(self, sy, an, mn, **kwargs):
+        """Initiate symbol, atomic number, mass number and kewords."""
         super(Nuclide, self).__init__()
         self.atomic_symbol = sy
         self.atomic_number = an
@@ -33,9 +33,7 @@ class Nuclide(object):
         self.stability()
 
     def nuclide_setter(self, kwargs):
-        """
-        Interprets kwargs (adds atomic mass, isotopic abundance)
-        """
+        """Interpret kwargs (adds atomic mass, isotopic abundance)."""
         if "atomic_mass" in kwargs:
             self.atomic_mass = kwargs["atomic_mass"]
         else:
@@ -46,9 +44,7 @@ class Nuclide(object):
             self.abundance = FLOAT(0)
 
     def stability(self):
-        """
-        Checks if the isotope is stable.
-        """
+        """Check if the isotope is stable."""
         self.is_stable = False
         if self.abundance > 0:
             self.is_stable = True
@@ -56,9 +52,10 @@ class Nuclide(object):
 
 class Element(object):
     """
-    This contains properties of a given element
-      Variables: symbol, number, mass, weight, representing_isotope, is_stable
-      Methods: isotope(mass_number)
+    This contains properties of a given element.
+
+    Variables: symbol, number, mass, weight, representing_isotope, is_stable
+    Methods: isotope(mass_number)
     """
 
     def __init__(self, sy, an, **kwargs):
@@ -73,8 +70,9 @@ class Element(object):
 
     def element_setter(self, kwargs):
         """
-        Interprets kwargs (adds atomic mass, isotopic abundance and if it is
-         stable).
+        Interpret kwargs.
+
+        Adds atomic mass, isotopic abundance and if it is stable.
         """
         if "isotopes" in kwargs:
             self.isotopes = kwargs["isotopes"]
@@ -86,9 +84,7 @@ class Element(object):
             self.atomic_weight_str = ''
 
     def isotope(self, mass_number):
-        """
-        Returns an nuclide object of a given isotope
-        """
+        """Return an nuclide object of a given isotope."""
         for isotope in self.isotopes:
             if isotope.mass_number == mass_number:
                 return isotope
@@ -96,9 +92,7 @@ class Element(object):
                                                    mass_number))
 
     def stability(self):
-        """
-        Checks if the element is stable.
-        """
+        """Check if the element is stable."""
         for isotope in self.isotopes:
             if isotope.abundance > 0.0:
                 return True
@@ -106,10 +100,12 @@ class Element(object):
 
     def calculate_atomic_weight(self, mode='average'):
         """
-        Returns the atomic weight which is either a weighted average over the
-         stable isotopes (mode='average'), the average of the range given in
-         the data file (mode='range') or in case of an unstable element the
-         mass of the most stable isotope.
+        Return the atomic weight.
+
+        This is either a weighted average over the stable isotopes
+         (mode='average'), the average of the range given in the data file
+         (mode='range') or in case of an unstable element the mass of the most
+         stable isotope.
         """
         if self.is_stable:
             if mode == 'average':
@@ -139,10 +135,11 @@ class Element(object):
 
     def get_representative_isotope(self):
         """
+        Return a representing isotope (nuclide class).
+
         Depending on whether or not the element is stable, the representing
          isotope is either the most abundant or the one with the longest
          half-life.
-        Returns a nuclide class.
         """
         if self.is_stable:
             check = [0, 0.0]
@@ -165,7 +162,8 @@ class Element(object):
 
 class PeriodicTableNIST(object):
     """
-    Reads the Periodic Table information from the provided NIST DATAFILE
+    Reads the Periodic Table information from the provided NIST DATAFILE.
+
     This is slow and ugly! Hence this is only used for the initial database
     creation.
       Variables: an_to_sy, sy_to_an
@@ -179,7 +177,8 @@ class PeriodicTableNIST(object):
 
     def element(self, element):
         """
-        Returns the corresponding class to an element.
+        Return the corresponding class to an element.
+
         The variable element can be int (atomic number) or str (symbol).
         """
         try:
@@ -197,9 +196,7 @@ class PeriodicTableNIST(object):
         return Element(sy, an, isotopes=isotopes, atomic_weight_str=aw_str)
 
     def atomic_number_to_symbol(self):
-        """
-        Returns a dictionary mapping the atomic numbers to the symbols.
-        """
+        """Return a dictionary mapping the atomic numbers to the symbols."""
         re_atomic_number = re.compile('Atomic Number = (\d+)')
         re_atomic_symbol = re.compile('Atomic Symbol = (\w+)')
         element_map = []
@@ -216,17 +213,16 @@ class PeriodicTableNIST(object):
         return OrderedDict(element_map)
 
     def symbol_to_atomic_number(self):
-        """
-        Returns a dictionary mapping the the atomic symbols to the numbers.
-        """
+        """Return a dictionary mapping the the atomic symbols to the numbers."""
         return OrderedDict((sy, an) for an, sy in self.an_to_sy.items())
 
     def element_isotopic_data(self, atomic_num):
-        '''
-        Returns a lost for an atomic_number containing all the isotopes
-         which in turn are Nuclide class containing masses and abundances.
+        """
+        Return a list containing all the isotopes for an atomic_number.
+
+        This in turn is Nuclide class containing masses and abundances.
         Also returns the NIST standard weight string.
-        '''
+        """
         re_atomic_number = re.compile('Atomic Number = (\d+)')
         re_atomic_symbol = re.compile('Atomic Symbol = (\w+)')
         re_mass_number = re.compile('Mass Number = (\d+)')
@@ -285,7 +281,8 @@ class PeriodicTableNIST(object):
 
     def read_data_file(self):
         """
-        Returns the data file as an string.
+        Return the data file as an string.
+
         The file is always small, so this is ok and makes everything faster.
         """
         with open(DATAFILE) as data_raw:
@@ -294,9 +291,7 @@ class PeriodicTableNIST(object):
 
 
 class PeriodicTable(object):
-    """
-    Reads the Periodic Table information from the provided HDF5 DATAFILE
-    """
+    """Reads the Periodic Table information from the provided HDF5 DATAFILE."""
 
     def __init__(self):
         super(PeriodicTable, self).__init__()
@@ -308,25 +303,23 @@ class PeriodicTable(object):
             sys.exit("The file {} is missing.".format(HDF5FILE))
 
     def __del__(self):
+        """Make sure the HDF5 is closed upon cleanup."""
         self.hdf5.close()
 
     def atomic_number_to_symbol(self):
-        """
-        Returns a dictionary mapping the atomic numbers to the symbols.
-        """
+        """Return a dictionary mapping the atomic numbers to the symbols."""
         el_map = [[el.attrs['number'], sy] for sy, el in self.hdf5.items()]
         el_map.sort(key=operator.itemgetter(0))
         return OrderedDict(el_map)
 
     def symbol_to_atomic_number(self):
-        """
-        Returns a dictionary mapping the the atomic symbols to the numbers.
-        """
+        """Return a dictionary mapping the the atomic symbols to the numbers."""
         return OrderedDict((sy, an) for an, sy in self.an_to_sy.items())
 
     def element(self, element):
         """
-        Imports the generated hdf5 version of the Periodic table data
+        Import the generated hdf5 version of the Periodic table data.
+
         We are reading:
             Nuclides:
                 atomic_symbol:      str
@@ -371,9 +364,7 @@ class PeriodicTable(object):
 
 
 class ExportPeriodicTableNIST(object):
-    """
-    This class harbours the export functions for the PeriodicTable class
-    """
+    """This class harbours the export functions for the PeriodicTable class."""
 
     def __init__(self):
         super(ExportPeriodicTableNIST, self).__init__()
@@ -381,7 +372,8 @@ class ExportPeriodicTableNIST(object):
 
     def print_masses_cpp_array(self):
         """
-        Prints the representative masses for export to an ugly C++ array.
+        Print the representative masses for export to an ugly C++ array.
+
         I'm looking at you Orca!
         """
         header = "    //{0:^10}       {1:^3}  {2:<4}   {3:^12}\n".format(
@@ -397,8 +389,9 @@ class ExportPeriodicTableNIST(object):
 
     def export_elements_to_hdf5(self):
         """
-        Writes a hdf5 file dumping the elements data, which should
-         speed up subsequent loading of the element data.
+        Write a hdf5 file dumping the elements data.
+
+        This should speed up subsequent loading of the element data.
         We are dumping:
             Nuclides:
                 atomic_symbol:      str
@@ -444,9 +437,7 @@ class ExportPeriodicTableNIST(object):
             hdf5_ub.write(user_block_string)
 
     def write_header(self, f):
-        """
-        Adds some root attributes to an hdf5 object
-        """
+        """Add some root attributes to an hdf5 object."""
         from time import gmtime, strftime
 
         f.attrs['file_name'] = os.path.basename(HDF5FILE)
