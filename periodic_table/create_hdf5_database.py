2955715c869f9173f5703080461c55f2763c0a11
Jalo D. Capa <jalo.capa@gmail.com>
2016-05-03 11:28:02 +0200

    Separation of database usage and database creation.

--- periodic_table/create_hdf5_database.py
+++ periodic_table/create_hdf5_database.py
@@ -1,5 +1,5 @@
 # Just so one can update the HDF5 database if the NIST data is changed.
-from periodictable import ExportPeriodicTableNIST
+from database_tools import ExportPeriodicTableNIST
 
 EPTN = ExportPeriodicTableNIST()
 EPTN.export_elements_to_hdf5()
