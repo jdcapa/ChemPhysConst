e93a63f09091ce21cee55c3ccab93b178099986f
Jalo D. Capa <jalo.capa@gmail.com>
2016-05-03 12:49:42 +0200

    Version: 1.0 --> 1.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0',
+    version='1.1',
 
     description=('Python library of atomic weights and isotopic ' +
                  'compositions for all known chemical elements.'),
