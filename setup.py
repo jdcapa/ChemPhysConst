4dbbf6af4c60ec0eee9ed3eee15f5be51ebfbb40
Jalo D. Capa <jalo.capa@gmail.com>
2016-05-03 11:29:17 +0200

    Removed the *.dat file from the setup

--- setup.py
+++ setup.py
@@ -100,8 +100,7 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'periodic_table': ['data/*.dat',
-                           'data/*.hdf5'],
+        'periodic_table': ['data/*.hdf5'],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
